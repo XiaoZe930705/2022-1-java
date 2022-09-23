8f24ff8e8bed11338867c12535be12111af90d17
XiaoZe930705 <113684234+XiaoZe930705@users.noreply.github.com>
2022-09-23 11:57:52 +0800

    2022-2

--- Test.java
+++ Test.java
@@ -1,5 +1,10 @@
 public class Test {
-    public static void main(String[] args){
-	    System.out.println("Second Version");
+    public static void main(String[] args) {
+	    System.out.println(args[0]);
+	    printStr("XiaoZe");
+    }
+    
+    public static void printStr(String str) {
+        System.out.println("My Name = " + str);
     }
 }
