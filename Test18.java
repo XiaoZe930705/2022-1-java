5cf46bbfe6722f96ee06a2400559703beb605489
XiaoZe930705 <113684234+XiaoZe930705@users.noreply.github.com>
2022-10-28 11:00:23 +0800

    2022-10-28

---
+++ Test18.java
@@ -0,0 +1,15 @@
+public class Test17{
+    public static void main (String[] avgs){
+        for(int i =0 ;i <= 5; i++){
+            for(int j = -4; i - 4 + 5 ; j++){
+                if(j < 1 ){
+                System.out.printf("*");
+                }
+                else {
+                    System.out.printf("%d",-(4-j-i));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
