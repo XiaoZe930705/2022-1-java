2df31dfe3c2f23fe02799ca59ddfe2da9acd6fff
XiaoZe930705 <113684234+XiaoZe930705@users.noreply.github.com>
2022-12-02 11:24:15 +0800

    2022/11/25

---
+++ Test24.java
@@ -0,0 +1,14 @@
+//副程式教學範例
+public class Test24{
+    public static void main (String[] args){
+        Test25 t25 = new Test25();
+        int a= t25.add(3,4);
+        System.out.printf("%d", a);
+    }
+}
+   
+/*public class Test25{//副檔名
+    public static int add(int x , int y){
+        return x+y;
+    }
+}*/
